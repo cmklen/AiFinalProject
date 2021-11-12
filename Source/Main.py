b5e165bda67dc70787aeb2b2fa7684d7b0a5c497
cmklen <cmklen01@louisville.edu>
2021-11-12 17:03:22 -0500

    ninja fix

--- Source/Main.py
+++ Source/Main.py
@@ -9,7 +9,7 @@ import GUI as gui
 coolGuy = gen.Genetic(mutRate=.008, mutType="Random", popSize=10, crossType="Crossy", cutoff=5, gridSize=7)
 
 #Get directory that the test files are in
-TestPath = "../TestData/"
+TestPath = "TestData/"
 
 numberNeeded = coolGuy.PopulateGrid(TestPath, "7x7.txt")
 
