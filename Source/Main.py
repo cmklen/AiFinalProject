d067c5b9c3eedefb1ba34e197968a2c0b5ee6690
Dbender01 <drewbender0010@gmail.com>
2021-11-14 16:25:38 -0500

    added function to lower fitness if paths are broken

--- Source/Main.py
+++ Source/Main.py
@@ -6,13 +6,15 @@ import Genetic as gen
 import os
 import GUI as gui
 
-coolGuy = gen.Genetic(mutRate=.008, mutType="Random", popSize=150, crossType="Crossy", cutoff=200, gridSize=7)
+coolGuy = gen.Genetic(mutRate=.05, mutType="Random", popSize=300, crossType="Crossy", cutoff=1000, gridSize=7)
 
 #Get directory that the test files are in
 TestPath = "TestData/"
 
 coolGuy.PopulateGrid(TestPath, "7x7.txt")
 
+
 finalGen = coolGuy.RunAlgorithm()
+print("Number of Pokie dots: ",coolGuy.CountStrayPath(finalGen[0][0]))
+
 gui.DisplayGame(finalGen[0][0], coolGuy.grid, coolGuy.GetNumberOfNumbers())
-print()
