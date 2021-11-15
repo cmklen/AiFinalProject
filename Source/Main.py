662953ec11c879baf04f8dedb1083e0cac952395
cmklen <cmklen01@louisville.edu>
2021-11-15 13:41:52 -0500

    connect things better

--- Source/Main.py
+++ Source/Main.py
@@ -6,7 +6,7 @@ import Genetic as gen
 import os
 import GUI as gui
 
-coolGuy = gen.Genetic(mutRate=.010, mutType="Random", popSize=150, crossType="Crossy", cutoff=200, gridSize=7)
+coolGuy = gen.Genetic(mutRate=.010, mutType="Random", popSize=10, crossType="Crossy", cutoff=200, gridSize=7)
 
 
 #Get directory that the test files are in
@@ -16,6 +16,6 @@ coolGuy.PopulateGrid(TestPath, "7x7.txt")
 
 
 finalGen = coolGuy.RunAlgorithm()
-print("Number of Pokie dots: ",coolGuy.CountStrayPath(finalGen[0][0]))
+print("Number of Pokie dots: ", coolGuy.CountStrayPath(finalGen[0][0]))
 
 gui.DisplayGame(finalGen[0][0], coolGuy.grid, coolGuy.GetNumberOfNumbers())
